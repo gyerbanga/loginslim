a036cf3b6d1372e2d280bf4109e7a0e0e49a29cc
Gaston <gyerbanga@innosys.fr>
2020-03-03 16:14:05 +0100

    bootstrap : paramètres de connexion db

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,7 +41,6 @@ $container['flash'] = function ($container){
     return new \Slim\Flash\Messages;
 };
 
-
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
         'cache' => false,
