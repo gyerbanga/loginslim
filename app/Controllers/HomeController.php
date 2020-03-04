cebb17ad28c0978d8200d157c6a9baf159e95d12
Gaston <gyerbanga@innosys.fr>
2020-03-04 15:16:23 +0100

    app : identation

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Slim\Views\Twig as View;
 
 class HomeController extends Controller
 {
+
     public function index($request, $response)
     {
         return $this->view->render($response, 'home.twig');
