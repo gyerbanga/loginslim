5ba103fd3271d0227728637979cef552dbf8ba6b
Gaston <gyerbanga@innosys.fr>
2020-03-05 10:15:00 +0100

    Models : création des membres

---
+++ app/Models/Members.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Members extends Model
+{
+
+     /**
+     * The table associated with the model.
+     * The primary key associated with the table.
+     * @var string
+     * @var string
+     */
+    protected $table = 'membres';
+    protected $primaryKey = 'id_membres';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'civilite',
+        'adresse',
+        'cp',
+        'tel1',
+        'tel2',
+        'mail',
+        'adhesion_date',
+    ];
+
+
+    public function setPassword($date){
+        $day=date('Y-m-d H:i:s');
+        $this->update([
+            'date' => $day
+        ]);
+    }
+}
