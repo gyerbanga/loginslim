618cf09ae9d96ee3e8147386a8192fded98666d6
Gaston <gyerbanga@innosys.fr>
2020-03-05 10:13:58 +0100

    bootstrap : ajout de paramètres de cookies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Respect\Validation\Validator as validator;
 
+
+//instanciation des valeurs par défaut
 $app = new \Slim\App([
     'settings' => [
         'displayErrorDetails' => true,
@@ -17,10 +19,18 @@ $app = new \Slim\App([
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
         ],
+        'debug' => true,
         'determineRouteBeforeAppMiddleware' => true,
         'addContentLengthHeader' => false,
+        'cookies.secure' => false,
+         'cookies.lifetime' => '20 minutes',
     ],
 ]);
+
+/* Configuration des éléments instanciés*/
+//http://www.slimframework.com/docs/v2/configuration/settings.html
+$app->config('cookies.lifetime', '20 minutes');
+$app->config('cookies.secure', false);
 
 $container = $app->getContainer();
 
