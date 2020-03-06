d6e93de513a6d18a7aabc3f3946dc5e5be841308
Gaston <gyerbanga@innosys.fr>
2020-03-06 13:29:55 +0100

    app : controller indentation

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -1,21 +1,24 @@
 <?php
+
 namespace App\Controllers;
 
+class Controller
+{
 
-class Controller{
     protected $container;
+
     public function __construct($container)
     {
         $this->container=$container;
     }
-
 
     public function __get($property)
     {
         /**
          * permet d'utiliser directement view-> à la place de container->view
          */
-        if ($this->container->{$property}){
+        if ($this->container->{$property})
+        {
             return $this->container->{$property};
         }
     }
