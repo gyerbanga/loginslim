4c856dcbb8f5b91dba2bdc56dbab56709368263c
Gaston <gyerbanga@innosys.fr>
2020-03-06 16:24:42 +0100

    app : ajout de nouvelles fonctionnalités

--- app/Auth/Membre.php
+++ app/Auth/Membre.php
@@ -14,4 +14,33 @@ class Membre
         $membres = Members::all();
         return $membres;
     }
+
+    //obtenir les membres à jour de cotisation
+    public function membresAjourCota(){
+
+    	$cotas=Members::select('id_membres','adhesion_date','nom','prenom','cotisation','dons')
+    					->where('cotisation','>',0)
+    					->get();
+    	//$cotas->select('nom','prenom','cotisation','dons')->get();
+    	return $cotas;
+    }
+
+    //les statistiques sur les membres
+    public function getStat(){
+
+    	// de ceux qui ont cotisé
+    	$count=Members::where('cotisation','>',0)->count();
+    	$maxCot=Members::where('cotisation','>',0)->max('cotisation');
+    	$minCot=Members::where('cotisation','>',0)->min('cotisation');
+    	$total=Members::where('cotisation','>',0)->sum('cotisation');
+        $moyenne=Members::where('cotisation','>',0)->avg('cotisation');
+
+    	return array(
+    		'effectif'=>$count,
+    		'cot_max'=>$maxCot,
+    		'total'=>$total,
+            'moyenne'=>$moyenne,
+    		'cota_min'=>$minCot
+    	);
+    }
 }
