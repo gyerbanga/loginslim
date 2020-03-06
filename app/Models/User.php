daba9f05bbee9343eb42529d086adb8206c5a445
Gaston <gyerbanga@innosys.fr>
2020-03-06 13:32:55 +0100

    app Models : identation user Modele

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+
+     /**
+     * The table associated with the model.
+     * The primary key associated with the table.
+     * @var string
+     * @var string
+     */
     protected $table = 'users';
+    protected $primaryKey = 'user_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
 
     protected $fillable = [
         'email',
