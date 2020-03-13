5fd0449ba977e987699e4d6bbd3194875fdabd51
Gaston <gyerbanga@innosys.fr>
2020-03-13 09:35:36 +0100

    app : joinctures dans la classe Membre

--- app/Auth/Membre.php
+++ app/Auth/Membre.php
@@ -1,8 +1,11 @@
 <?php
 
+/* https://code.tutsplus.com/tutorials/using-illuminate-database-with-eloquent-in-your-php-app-without-laravel--cms-27247 */
+/* https://laravel.com/docs/6.x/eloquent-relationships */
+
 namespace App\Auth;
 
-use App\Models\Members;
+use App\Models\Members
 
 class Membre
 {
@@ -12,19 +15,36 @@ class Membre
     {
     	//$this->adhesion_date= $this->adhesion_date=='' ? $day : $this->adhesion_date;
         $membres = Members::all();
+        /* $livres = Members::find(3)->bureau; */
+
         return $membres;
     }
 
+    //récupérer l'enregistrement qui a la clé primaire donnée en paramètre
+    public function certainsElts($int)
+    {
+        //$int pourrait être aussi un tableau de valeur [1,5,9]
+        return Members::find($int);
+    }
+
+    //récupérer liste liées à un paramètre
+    public function lister($nom)
+    {
+        return Members::lists($nom);
+    }
     //obtenir les membres à jour de cotisation
     public function membresAjourCota(){
 
     	$cotas=Members::select('id_membres','adhesion_date','nom','prenom','cotisation','dons')
     					->where('cotisation','>',0)
+                        ->distinct()
     					->get();
     	//$cotas->select('nom','prenom','cotisation','dons')->get();
     	return $cotas;
     }
 
+    //Les jointures
+   // $bureaux = Members::find(11)->bureau;
     //les statistiques sur les membres
     public function getStat(){
 
